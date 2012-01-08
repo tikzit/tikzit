bece3143036f939e72c7ec9a3cb20446058514a3
randomguy3 <randomguy3@7c02a99a-9b00-45e3-bf44-6f3dd7fddb64>
2012-01-08 23:23:27 +0000

    Fix windows link on website
    git-svn-id: https://tikzit.svn.sourceforge.net/svnroot/tikzit/trunk@361 7c02a99a-9b00-45e3-bf44-6f3dd7fddb64

--- docs/web/link.php
+++ docs/web/link.php
@@ -1,13 +1,13 @@
 <?
 $dmg_ver = "0.4";
 $src_ver = "0.7";
-$win_ver = "0.3";
+$win_ver = "0.7";
 $file_root = "http://sourceforge.net/projects/tikzit/files";
 
 $urls = array(
 'src' => "$file_root/tikzit-$src_ver/tikzit-$src_ver.tar.gz/download",
 'dmg' => "$file_root/tikzit-$dmg_ver/TikZiT-$dmg_ver.dmg/download",
-'win' => "$file_root/tikzit-$win_ver/tikzit-$win_ver.zip/download"
+'win' => "$file_root/tikzit-$win_ver/tikzit-setup-$win_ver.exe/download"
 );
 
 
