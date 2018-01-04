ae3f1cfde0626a61c3968a8d797a22ed9fd16175
Aleks Kissinger <aleks0@gmail.com>
2018-01-04 16:24:44 +0100

    removed some build artefacts from repo

--- target_wrapper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-DYLD_FRAMEWORK_PATH=/usr/local/Cellar/qt5/5.7.1_1/lib${DYLD_FRAMEWORK_PATH:+:$DYLD_FRAMEWORK_PATH}
-export DYLD_FRAMEWORK_PATH
-QT_PLUGIN_PATH=/usr/local/Cellar/qt5/5.7.1_1/plugins${QT_PLUGIN_PATH:+:$QT_PLUGIN_PATH}
-export QT_PLUGIN_PATH
-exec "$@"
