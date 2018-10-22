253df5f1d9e8915dfc62261dd338b96014dc1984
Aleks Kissinger <aleks0@gmail.com>
2018-10-22 20:14:15 +0200

    updated windows deploy script

--- deploy-win.bat
+++ deploy-win.bat
@@ -2,6 +2,16 @@ mkdir dist
 cd dist
 mkdir tikzit
 cd tikzit
-cp ..\..\tikzit.exe .
+mkdir icons
 
-windeployqt.exe --no-webkit2 --no-angle --no-opengl-sw --no-system-d3d-compiler --no-translations --no-quick-import .\tikzit.exe
+copy ..\..\tikzfiles.reg .
+copy ..\..\release\tikzit.exe .
+copy ..\..\images\tikzdoc.ico icons\
+copy C:\Windows\System32\msvcp140.dll .
+copy C:\Windows\System32\vcruntime140.dll .
+
+windeployqt.exe --no-compiler-runtime --no-webkit2 --no-angle --no-opengl-sw --no-system-d3d-compiler --no-translations --no-quick-import .\tikzit.exe
+
+cd ..
+7z a -tzip tikzit.zip tikzit
+cd ..
