871f080501b883b64f8d7c38c725cfa006b13981
Aleks Kissinger <aleks0@gmail.com>
2018-12-21 19:23:26 +0100

    broke dmg into 2 steps

--- deploy-osx.sh
+++ deploy-osx.sh
@@ -1,5 +1,7 @@
 # deploy the Mac app bundle. Note the bin/ directory
 # of Qt should be in your PATH
 
-macdeployqt tikzit.app -dmg
+macdeployqt tikzit.app
+hdiutil create -volname TikZiT -srcfolder tikzit.app -ov -format UDZO tikzit.dmg
+
 
