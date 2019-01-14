4b062a6a99a31b34e5a1556680f27369d690dc7b
Aleks Kissinger <aleks0@gmail.com>
2019-01-14 13:05:07 +0100

    ...

---
+++ scripts/gh-get.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from gh import gh, pr
+import sys, os
+
+filter = None
+if len(sys.argv) == 2:
+	filter = sys.argv[1]
+
+tok = os.getenv('GITHUB_TOKEN')
+
+draft = [r for r in gh('releases') if r['draft']][0]
+
+for a in draft['assets']:
+	if filter == None or filter in a['name']:
+		print('Downloading ' + a['name'])
+		b = gh('releases/assets/%s' % a['id'],
+			   ['-L', '-H', 'Accept: application/octet-stream'],
+			   parse=False, quiet=False)
+		f = open(a['name'], 'w')
+		f.write(b)
+		f.close()
